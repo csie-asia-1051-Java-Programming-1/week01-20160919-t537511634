87bbf24a09dca88e951e002d6f530133387f6980
t537511634 <105021029@live.asia.edu.tw>
2016-09-19 16:20:06 +0800

    wk01-hw03 admit

--- week01_20160919/src/hw/hw03_105021029.java
+++ week01_20160919/src/hw/hw03_105021029.java
@@ -11,6 +11,20 @@ public class hw03_105021029 {
 		// TODO Auto-generated method stub
 
 		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入6個數(可以小有數) :");
+		float a = scn.nextInt();
+		float b = scn.nextInt();
+		float c = scn.nextInt();
+		float d = scn.nextInt();
+		float e = scn.nextInt();
+		float f = scn.nextInt();
+		float g = Math.min(a, b);
+		float h = Math.min(b, g);
+		float i = Math.min(c, h);
+		float j = Math.min(d, b);
+		float k = Math.min(e, j);
+		float l = Math.min(f, k);
+		System.out.println("每一個值都取四捨五入後輸出這 6 個數中的最小值為:"+ l);
 	}
 
 }
