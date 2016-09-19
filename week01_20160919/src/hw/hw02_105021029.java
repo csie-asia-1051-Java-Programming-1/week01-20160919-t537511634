9865a14c10b90040127f2c4aa6f773549d522390
t537511634 <105021029@live.asia.edu.tw>
2016-09-19 15:58:12 +0800

    wk01-hw備份

---
+++ week01_20160919/src/hw/hw02_105021029.java
@@ -0,0 +1,18 @@
+package hw;
+/*
+ * Topic:請寫一個程式讓使用者輸入 n 並產生 6 個介於 1~n 之間的亂數 (輸出的亂數可重覆)
+ * Date: 2016/09/19
+ * Author: 105021029 汪元浩
+ */
+import java.util.Random;
+import java.util.Scanner;
+public class hw02_105021029 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		Scanner rnd = new Random();
+	}
+
+}
---
+++ week01_20160919/src/hw/hw03_105021029.java
@@ -0,0 +1,16 @@
+package hw;
+/*
+ * Topic: 請寫一個程式讓使用者輸入 6 個數(可以有小數)的值，每一個值都取四捨五入後輸出這 6 個數中的最小值
+ * Date: 2016/09/19
+ * Author: 105021029 汪元浩
+ */
+import java.util.Scanner;
+public class hw03_105021029 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+	}
+
+}
