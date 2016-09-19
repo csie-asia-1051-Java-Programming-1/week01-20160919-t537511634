44428f479e933933eb71633044a375da4c1f3ea1
t537511634 <105021029@live.asia.edu.tw>
2016-09-19 16:08:58 +0800

    wk01-hw02 繳交

--- week01_20160919/src/hw/hw02_105021029.java
+++ week01_20160919/src/hw/hw02_105021029.java
@@ -15,6 +15,18 @@ public class hw02_105021029 {
 		Random rnd = new Random();
 		System.out.println(" 輸入一個數字n:");
 		int n = scn.nextInt();
+		int v1 =rnd.nextInt(n)+1;
+		int v2 =rnd.nextInt(n)+1;
+		int v3 =rnd.nextInt(n)+1;
+		int v4 =rnd.nextInt(n)+1;
+		int v5 =rnd.nextInt(n)+1;
+		int v6 =rnd.nextInt(n)+1;
+		System.out.println( "6 個介於 1~n 之間的亂數為:" + v1);
+		System.out.println( v2);
+		System.out.println( v3);
+		System.out.println( v4);
+		System.out.println( v5);
+		System.out.println( v6);
 	}
 
 }
